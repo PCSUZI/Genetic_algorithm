b8d1eaa12d85f90c205efc14ea66e44bd1644abf
green <pc_suzi@youcanstar.com>
2019-12-17 15:07:32 +0900

    add *

--- Assets/04. Sprites/Reset.cs
+++ Assets/04. Sprites/Reset.cs
@@ -7,16 +7,19 @@ public class Reset : MonoBehaviour
     Transform tr;
 
     Vector3 m_startPos;
+    Vector3 m_startRot;
    
     // Start is called before the first frame update
     void Start()
     {
         tr = gameObject.GetComponent<Transform>();
-        m_startPos = gameObject.transform.position;
+        m_startPos = tr.position;
+        m_startRot = tr.rotation.eulerAngles;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         tr.position = m_startPos;
+        tr.rotation = Quaternion.Euler(m_startRot);
     }
 }
